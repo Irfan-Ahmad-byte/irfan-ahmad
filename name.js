e6b2eec672d5f7cc5a729304ee2875030925ad5a
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2021-06-26 12:33:09 +0500

    added navbar

--- name.js
+++ name.js
@@ -1,20 +1,4 @@
-function btnClick () {
-	document.getElementById("home").style.display = "none";
-	var about = document.getElementById("about-sec");
-	about.scrollIntoView();
-	about.style.display = "block";
-	about.style.position = "absolute";
-	about.style.top = "0%";
-	about.style.left = "0%";
-	var nav = document.getElementById("navigation-left");
-	nav.style.position = "absolute";
-	// nav.style.transform = "translateY(20vw)"
-	nav.style.float = "left"
-	var nav1 = document.getElementById("navigation-right");
-	// nav1.style.position = "absolute";
-	nav1.style.float = "left"
-	// nav1.style.transform = "translateY(20vw)"
-}
+
 
 var coll = document.getElementsByClassName("title");
 var i;
