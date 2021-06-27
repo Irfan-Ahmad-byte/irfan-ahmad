35eb91142d319aff1138608631f868cc37184b89
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2021-06-27 16:42:19 +0500

    changes

--- name.js
+++ name.js
@@ -1,4 +1,4 @@
-
+// left margin of home btn
 var screenwidth = screen.width;
 var homebtn = document.getElementById("home-btn");
 var btnwidth = homebtn.offsetWidth;
@@ -7,6 +7,46 @@ var btnhalf = btnwidth/2;
 var marginleft = screenhalf - btnhalf;
 
 homebtn.style.marginLeft = marginleft + "px";
+
+// test functions
+function moreClick () {
+	var sec = document.getElementsByClassName("nav-link");
+	var sec1 = document.getElementById("about-sec");	
+	alert(sec[6].offsetTop);
+	alert(sec1.offsetTop);
+	alert(window.pageYOffset);
+	alert(document.documentElement.scrollHeight);
+}
+
+
+// change svg fill color when background matches the fill
+// var bgcolor = document.getElementById("about-sec").style.backgroundColor;
+// var svgs = document.getElementsByClassName("hover-svg");
+// var docheight = document.documentElement.scrollHeight;
+// do {
+// 	for (i=0; i < svgs.length; i++) {
+// 		// height of the svg
+// 		var svgheight = svgs[i].offsetTop;
+// 		// height of about section
+// 		var aboutheight = document.getElementById("about-sec").offsetTop;
+// 		// scroll position
+// 		var scrlheight = window.pageYOffset;
+// 		// height of about section from top
+// 		var hight = aboutheight - scrlheight;
+// 		// height of next section
+// 		var nexthight = document.getElementById("works").offsetTop;
+// 		// change color if same
+// 		var fill = svgs[i].style.fill;
+// 		if (hight <= svgheight && hight > 0) {
+// 				if (fill == bgcolor) {
+// 					svgs[i].style.fill = '#DC2B1C';
+// 			};
+// 		} else {
+// 			svgs[i].style.fill = fill;
+// 		};
+
+// 	};
+// } while (scrlheight < docheight);
 
 var coll = document.getElementsByClassName("title");
 var i;
