67a105443a2f98dc6fda8eb9fed91821ae7ccae7
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2021-06-28 12:34:29 +0500

    menu toggler improved

--- name.js
+++ name.js
@@ -1,53 +1,4 @@
-// left margin of home btn
-// var screenwidth = screen.width;
-// var homebtn = document.getElementById("home-btn");
-// var btnwidth = homebtn.offsetWidth;
-// var screenhalf = screenwidth/2;
-// var btnhalf = btnwidth/2;
-// var marginleft = screenhalf - btnhalf + 50;
-
-// homebtn.style.marginLeft = marginleft + "px";
-
-// test functions
-// function moreClick () {
-// 	var sec = document.getElementsByClassName("nav-link");
-// 	var sec1 = document.getElementById("about-sec");	
-// 	alert(sec[6].offsetTop);
-// 	alert(sec1.offsetTop);
-// 	alert(window.pageYOffset);
-// 	alert(document.documentElement.scrollHeight);
-// }
-
-
-// change svg fill color when background matches the fill
-// var bgcolor = document.getElementById("about-sec").style.backgroundColor;
-// var svgs = document.getElementsByClassName("hover-svg");
-// var docheight = document.documentElement.scrollHeight;
-// do {
-// 	for (i=0; i < svgs.length; i++) {
-// 		// height of the svg
-// 		var svgheight = svgs[i].offsetTop;
-// 		// height of about section
-// 		var aboutheight = document.getElementById("about-sec").offsetTop;
-// 		// scroll position
-// 		var scrlheight = window.pageYOffset;
-// 		// height of about section from top
-// 		var hight = aboutheight - scrlheight;
-// 		// height of next section
-// 		var nexthight = document.getElementById("works").offsetTop;
-// 		// change color if same
-// 		var fill = svgs[i].style.fill;
-// 		if (hight <= svgheight && hight > 0) {
-// 				if (fill == bgcolor) {
-// 					svgs[i].style.fill = '#DC2B1C';
-// 			};
-// 		} else {
-// 			svgs[i].style.fill = fill;
-// 		};
-
-// 	};
-// } while (scrlheight < docheight);
-
+// menu toggler button
 var menu = document.getElementById("menu-toggle");
 menu.addEventListener("click", function() {
 	this.classList.toggle("active");
@@ -65,29 +16,40 @@ menu.addEventListener("click", function() {
 
 	if (rot2 == "none" || rot2 == "matrix(1, 0, 0, 1, 0, 0)") {
 		fs2.style.transform = "rotate(90deg)";
-		fs2.style.marginLeft = "10px";
+		fs2.style.marginLeft = "15px";
 	} else {
 		fs2.style.transform = "rotate(0deg)";
+		fs2.style.marginLeft = "0px";
 	}
 	if (rot1 == "none" || rot1 == "matrix(1, 0, 0, 1, 0, 0)") {
-		fs1.style.transform = "rotate(-55deg)";
+		fs1.style.transform = "rotate(-25deg)";
+		fs1.style.marginLeft = "2px";
 	} else {
 		fs1.style.transform = "rotate(0deg)";
+		fs1.style.marginLeft = "0px";
 	}
 	if (rot3 == "none" || rot3 == "matrix(1, 0, 0, 1, 0, 0)") {
-		fs3.style.transform = "rotate(55deg)";
+		fs3.style.transform = "rotate(25deg)";
+		fs3.style.marginLeft = "2px";
 	} else {
 		fs3.style.transform = "rotate(0deg)";
+		fs3.style.marginLeft = "0px";
 	}		
-	// alert(rot2 + rot1 + rot3);
+	
+	let navElement = this.nextElementSibling;
+	if (navElement.style.display == "block") {
+		navElement.style.display = "none";
+	} else {
+		navElement.style.display = "block";
+	}
 
 });
 
-
-var coll = document.getElementsByClassName("proj-title");
+// project viewer
+var projects = document.getElementsByClassName("proj-title");
 var i;
-for (i=0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
+for (i=0; i < projects.length; i++) {
+	projects[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 		var content = this.nextElementSibling;
 		if (content.style.display == "block") {
